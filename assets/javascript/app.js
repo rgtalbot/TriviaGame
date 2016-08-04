e3702bd35478cd7243ef77c8ba5d59fbb24be791
rgtalbot <rgtalbot@users.noreply.github.com>
2016-08-04 17:40:43 -0400

    forgot to change the number of questions back to 10

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
             randomizedArray = triviaQuestions.sort(function(a, b) {
                 return 0.5 - Math.random();
             });
-            for (var i = 0; i < 2; i++) {
+            for (var i = 0; i < 10; i++) {
                 trivia.push(randomizedArray.pop());
             }
         },
